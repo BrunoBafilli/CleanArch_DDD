8ce48741708aeef9297a64abed6b436fab7f0eed
BrunoBafilli <brunobafilli@gmail.com>
2024-06-09 16:02:25 -0300

    teste
    teste

--- Desafio/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ Desafio/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48bc410fc397944855794af9e1f6fc2def80d5d5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e1c0c07199e4ddc6ffcfc14b4f04163bf216aa31")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
